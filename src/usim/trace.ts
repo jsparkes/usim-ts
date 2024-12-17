0563720057c318903668ac92f674234d83488657
Jeff Sparkes <jsparkes@opentext.com>
2024-12-17 14:41:04 -0500

    Add IOB level and format.

--- src/usim/trace.ts
+++ src/usim/trace.ts
@@ -4,6 +4,7 @@ const log: Logger<ILogObj> = new Logger();
 
 export const ANY = "ANY";
 export const DISK = "DISK";
+export const IOB = "IOB";
 export const INT = "INT";
 export const MICROCODE = "MICROCODE";
 export const MISC = "MISC";
@@ -12,14 +13,14 @@ export const UCODE = "UCODE";
 export const UNIBUS = "UNIBUS";
 export const USIM = "USIM";
 
-export function set_trace_level(level: string) {}
+export function set_trace_level(level: string) { }
 
-export function set_trace_facilities(target: string[]) {}
+export function set_trace_facilities(target: string[]) { }
 
-export function error(flag: string, msg: string) {}
+export function error(flag: string, msg: string) { }
 
-export function info(flag: string, msg: string) {}
+export function info(flag: string, msg: string) { }
 
-export function warning(flags: string, msg: string) {}
+export function warning(flags: string, msg: string) { }
 
-export function debug(flags: string, msg: string) {}
+export function debug(flags: string, msg: string) { }
